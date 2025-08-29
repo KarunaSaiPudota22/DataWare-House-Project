90d32c4c3c60d0fb5389fdb0f571a12dca5eef13
KarunaSaiPudota22 <pudotakarunasai@gmail.com>
2025-08-29 23:40:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# 📊 Data Warehouse & Analytics Project
+
+Welcome to the **Data Warehouse and Analytics Project** repository! 🚀  
+This portfolio project demonstrates a complete data warehousing and analytics solution—from raw data ingestion to business-ready insights. It follows industry best practices in **data engineering**, **ETL**, and **analytics**.
+
+---
+
+## 🏗️ Data Architecture Overview
+
+This project uses the **Medallion Architecture**, which organizes data into three layers:
+
+1. **Bronze Layer**  
+   - Raw data ingested from CSV files  
+   - Stored in SQL Server without transformation
+
+2. **Silver Layer**  
+   - Cleansed and standardized data  
+   - Includes normalization and business logic
+
+3. **Gold Layer**  
+   - Final analytical layer  
+   - Star schema with dimension and fact views for reporting
+![alt text](docs/data_architecture.png)
+
+
+---
+
+## 📖 Project Highlights
+
+This project includes:
+
+- ✅ Modern Data Architecture using Bronze, Silver, and Gold layers  
+- 🔄 ETL Pipelines to extract, transform, and load data from ERP and CRM sources  
+- 🧠 Data Modeling with optimized fact and dimension tables  
+- 📈 SQL-Based Analytics for insights into customer behavior, product performance, and sales trends
+
+### 💼 Skills Demonstrated
+- SQL Development  
+- Data Engineering  
+- ETL Pipeline Design  
+- Data Modeling  
+- Business Intelligence & Reporting  
+
+---
+
+## 🛠️ Tools & Resources
+
+All tools used are free and accessible:
+
+- 📁 **Datasets**: CSV files used in the project  
+- 🗄️ SQL Server Express  
+- 🧠 [SQL Server Management Studio (SSMS)](https://learn.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver16)  
+- 🧰 GitHub  
+- 🧩 DrawIO  
+- 📋 Notion  
+- 📌 Notion Project Steps
+
+---
+
+## 🚀 Project Goals
+
+### 🔧 Data Engineering
+
+**Objective**: Build a modern data warehouse using SQL Server to consolidate and prepare sales data for analysis.
+
+** a unified analytical model  
+- Focus on current data (no historization)  
+- Document the data model for business and analytics teams
+
+---
+
+### 📊 Data Analysis & Reporting
+
+**Objective**: Generate actionable insights using SQL-based analytics.
+
+**Focus Areas**:
+- Customer behavior analysis  
+- Product performance tracking  
+- Sales trend evaluation  
+
+These insights support strategic decision-making for stakeholders.
+
+---
+
+
---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,101 @@
+/*
+===============================================================================
+Script Name: Bronze Layer Table Creation (DDL)
+===============================================================================
+Purpose:
+    This script defines the Data Definition Language (DDL) for creating tables 
+    in the 'bronze' schema of a data warehouse architecture. The bronze layer 
+    typically serves as the raw ingestion layer, capturing untransformed data 
+    from various source systems.
+
+    Key Features:
+    - Drops existing tables if they already exist to ensure a clean re-creation.
+    - Creates structured tables for CRM and ERP data sources.
+    - Supports reusability and consistency in schema deployment.
+
+Usage:
+    Run this script during initial setup or when resetting the bronze layer 
+    structure to accommodate schema changes or data source updates.
+===============================================================================
+*/
+
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
+
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
